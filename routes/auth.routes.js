667af1001dc0785e3ac56167eafbfcd2451e50fd
MrMiM-tfe <mahdikhakbaz7@gmail.com>
2022-10-26 18:58:56 +0330

    update docs and bug fixe

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,9 +4,14 @@ const router = express.Router()
 // Controllers
 const { authController } = require('../controllers')
 
-router.get('/getuser', authController.getUser)
+const { protect } = require('../middlewares/auth')
+
 router.post('/register', authController.signup)
 router.post('/login', authController.login)
+
+router.use(protect)
+
+router.get('/getuser', authController.getUser)
 router.delete('/logout', authController.logout)
 
 module.exports = router
