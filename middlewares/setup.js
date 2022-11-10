3324ccb849df94274125fcb5befb5bacf568a8e6
MrMiM-tfe <mahdikhakbaz7@gmail.com>
2022-11-10 14:51:30 +0330

    fix

--- middlewares/setup.js
+++ middlewares/setup.js
@@ -4,7 +4,7 @@ const { Keyv } = require('../models')
 const checkIsFirstLoad = async (req, res, next) => {
     const isLoaded = await Keyv.findOne({key: "loaded"})
 
-    if (isLoaded){
+    if (isLoaded.value !== "true"){
         return res.status(401).json({
             type: "Error",
             message: "setup is complited in past"
@@ -18,7 +18,9 @@ const checkIsFirstLoad = async (req, res, next) => {
 const checkSetup = async (req, res, next) => {
     const isLoaded = await Keyv.findOne({key: "loaded"})
 
-    if (isLoaded === "true"){
+    console.log(isLoaded);
+
+    if (isLoaded.value === "true"){
         next()
     }else{
         return res.status(401).json({
